a048157e0d72ee94a1001931d86c5c5a20d012d5
Agusmac <agusmac40@gmail.com>
2022-04-17 16:51:38 -0300

    Update NavHero.js

--- src/NavHero.js
+++ src/NavHero.js
@@ -1,21 +1,32 @@
 import React, { useState, } from 'react'
 import { useInView } from 'react-intersection-observer';
-
+import Swal from 'sweetalert2';
 
 const Hamburguer = "/images/icon-hamburger.svg"
 const Cross = "/images/icon-close.svg"
 
 
-const NavHero = ({ footView,alert }) => {
+const NavHero = ({ footView, alert }) => {
 
     const { ref, inView } = useInView({
         rootMargin: '-150px',
     });
 
-function double(){
-    setExpanded(!expanded)
-    alert()
-}
+    function double(a) {
+        setExpanded(!expanded)
+        if (a) {
+            alert2()
+        } else {
+            alert()
+        }
+    }
+    function alert2() {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Work in progress...',
+        })
+    }
 
     const [expanded, setExpanded] = useState(false)
 
@@ -24,12 +35,12 @@ function double(){
         <>
             <div className='NavHero'>
                 <div className='Nav flex apart'>
-                    <div><img  src="/images/logo-light.png" alt="spaceImg" /></div>
+                    <div><img src="/images/logo-light.png" alt="spaceImg" /></div>
                     <img src={expanded ? Cross : Hamburguer} className={`burguer ${expanded ? "" : "wobble-ver-right"}`} alt="icon2" onClick={() => setExpanded(!expanded)} />
                     <div className={`navbar flex  ${expanded ? "display" : "displaynone"}`}>
                         <div onClick={() => setExpanded(!expanded)} className='selected'>HOME</div>
-                        <div onClick={() => setExpanded(!expanded)}>BLOG(W.I.P)</div>
-                        <div onClick={double}>CONTACT(W.I.P)</div>
+                        <div onClick={() => double(true)}>BLOG(W.I.P)</div>
+                        <div onClick={() => double(false)}>CONTACT(W.I.P)</div>
                     </div>
                 </div>
                 <div className='Hero'>
@@ -41,11 +52,11 @@ function double(){
                     <p>A team of passionate designers and developers from Yogyakarta</p>
                     <div className='fix' ref={ref}>
                         <div className='unfix'>
-                          
-                                <div onClick={alert} className={`btn btnWhite  ${inView ? "" : "btntransformed"}  ${footView ? "infootview" : ""}`}>
-                                    {inView ? `LET'S TALK!` : <i className="far fa-envelope"></i>}
-                                </div>
-                         
+
+                            <div onClick={alert} className={`btn btnWhite  ${inView ? "" : "btntransformed"}  ${footView ? "infootview" : ""}`}>
+                                {inView ? `LET'S TALK!` : <i className="far fa-envelope"></i>}
+                            </div>
+
                         </div>
                     </div>
                     <div className='flex JflexEnd hero3 displaynonemobile'><img src="/images/hero-guy-3.png" alt="hero3" /></div>
