e73bb0275e43c90f6e43d980fadfb85ac90dd331
Agusmac <agusmac40@gmail.com>
2022-05-05 11:00:48 -0300

    changed footer font sizes & margins

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -12,7 +12,7 @@ const Footer = ({ setFootView,alert }) => {
 
     return (
         <div className='footer grid ' style={{ placeContent: "center" }}>
-            <div>
+            <div className='spacerTop3'>
                 <h2 className='textcenter margincenter whiteT spacerTop3'>Interested in working with us?</h2>
                 <div ref={ref} onClick={alert} className='btn textcenter whiteT spacerTop3 spacerBot2' style={{ backgroundColor: "#ef4b6c" }}>LET'S TALK!</div>
                 <div className='flex footButtons marginCenter textcenter' style={{ justifyContent: "center" }}>
